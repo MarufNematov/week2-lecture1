3cf03cfeb98cee05d3425f84f7353e9a730d0a8d
Maruf <marufnegmatov107@gmail.com>
2024-03-19 13:37:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// function sum(n){                       //task1
+//     if(n==1) return 1
+     
+//     return n* sum(n-1)
+// }
+// console.log(sum(5));
+
+
+
+
+
+// function sum(n){                              //task2
+//     if(1==n) return n
+
+//     return n + sum (n-1)
+
+// }
+// console.log(sum(10));
+
+
+
+
+
+
+
+
+// function sum(n1, n2){                     //task3
+//     if(n2==0) return 1 
+
+//     return n1 * sum(n1, n2-1)
+// } 
+// console.log(sum(2,3));
+
+
+
+
+
+
+
